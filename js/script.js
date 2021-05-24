9f85c2faed25b3b17d45031973f35b86aadcdaa4
Karol Ariza <karolarizag@gmail.com>
2021-05-24 16:50:16 +0100

    first part finished

--- js/script.js
+++ js/script.js
@@ -1,61 +1,53 @@
-const img1 = document.getElementById('img1')
 
-const cards = document.querySelectorAll('[id^="img"]')
+const cards = document.querySelectorAll('[id^="img"]') // DOM card placeholders
 
-// console.log(cards)
+const arrCards = ['ice-cream', 'waffle', 'pancake', 'donut', 'ice-cream', 'waffle', 'pancake', 'donut'] // Card combination
 
-// evento on click
-function cardsClick (cards) {
+let matchCard = [] // Selected card pair
+
+
+function setLevel(level) {
+  var freePositions = [0, 1, 2, 3, 4, 5, 6, 7]
+
+  for (let i = 0; i < level.length; i++) {
+    var selected = Math.floor(Math.random() * freePositions.length)
+    cards[freePositions[selected]].classList.add(level[i])
+    cards[freePositions[selected]].style.visibility = 'visible'
+    freePositions.splice(selected, 1)
+  }
+
   cards.forEach(elem => {
     elem.onclick = function (e) {
-      selectCard(e.target)
+      selectCard(e.currentTarget)
     }
   })
 }
-const level1 = ['1', '2', '1', '2']
-const level2 = ['1', '2', '3', '4', '1', '2', '3', '4']
-const level3 = ['1', '2', '3', '4', '5', '6', '1', '2', '3', '4', '5', '6']
 
-const cardsLevel1 = []
-// enseñar cartas necesarias segun nivel
-let positionArray = []
-function setLevel (arr) {
-  const copy = arr.slice(0, arr.length)
-  positionArray = []
-  for (let i = 0; i < arr.length; i++) {
-    positionArray.push(cards[i])
-    cardsLevel1.push(cards[i])
+setLevel(arrCards)
 
-    cards[i].style.visibility = 'visible'
-    const randomCard = Math.floor(Math.random() * copy.length)
-    cards[i].childNodes[0].innerText = copy[randomCard]
-    copy.splice(randomCard, 1)
-  }
-
-  cardsClick(cardsLevel1)
-}
-setLevel(level2)
 // ----------------------------------------------
 
-let matchCard = []
-
-function selectCard (card) {
+function selectCard(card) {
   matchCard.push(card)
-  console.log(matchCard)
   if (matchCard.length === 2) {
     matchCard[0].style.pointerEvents = 'auto'
-    if (matchCard[0] !== matchCard[1]) {
-      if (matchCard[0].innerText === matchCard[1].innerText) {
-        console.log('coinciden')
-      } else {
-        console.log('no coinciden')
-      }
+    if (matchCard[0].getAttribute('class') === matchCard[1].getAttribute('class')) {
+      console.log('coinciden')
+      matchCard[0].style.pointerEvents = 'none'
+      matchCard[1].style.pointerEvents = 'none'
     } else {
-      console.log('pulsa otra carta')
+      console.log('no coinciden')
     }
     console.log(matchCard[0])
     matchCard = []
-  } else if (matchCard.length === 1){
+  } else if (matchCard.length === 1) {
     matchCard[0].style.pointerEvents = 'none'
   }
 }
+// -------------------------------------------------
+
+// function frontCard (elem) {
+//   if (elem.target.innerText === '1') {
+//     elem.classList.add('ice-cream')
+//   }
+// }
