7008852acf9b33ecc3d9c922ae85189228731d80
Karol Ariza <karolarizag@gmail.com>
2021-05-24 17:49:28 +0100

    condicion de ganador

--- js/script.js
+++ js/script.js
@@ -6,16 +6,17 @@ const arrCards = ['ice-cream', 'waffle', 'pancake', 'donut', 'ice-cream', 'waffl
 let matchCard = [] // Selected card pair
 
 
-function setLevel(level) {
-  var freePositions = [0, 1, 2, 3, 4, 5, 6, 7]
+function setLevel (level) { // damos colores a las cartas
+  let freePositions = [0, 1, 2, 3, 4, 5, 6, 7]
 
   for (let i = 0; i < level.length; i++) {
-    var selected = Math.floor(Math.random() * freePositions.length)
+    
+    let selected = Math.floor(Math.random() * freePositions.length)
     cards[freePositions[selected]].classList.add(level[i])
     cards[freePositions[selected]].style.visibility = 'visible'
     freePositions.splice(selected, 1)
   }
-
+  
   cards.forEach(elem => {
     elem.onclick = function (e) {
       selectCard(e.currentTarget)
@@ -25,18 +26,30 @@ function setLevel(level) {
 
 setLevel(arrCards)
 
+
 // ----------------------------------------------
 
-function selectCard(card) {
+
+let cont = 0
+let cont2 = 0
+function selectCard (card) {
   matchCard.push(card)
   if (matchCard.length === 2) {
     matchCard[0].style.pointerEvents = 'auto'
     if (matchCard[0].getAttribute('class') === matchCard[1].getAttribute('class')) {
       console.log('coinciden')
+      cont++
+      if (cont === 4) {
+        window.alert("YOU'RE ON FIRE!!!")
+      }
       matchCard[0].style.pointerEvents = 'none'
       matchCard[1].style.pointerEvents = 'none'
     } else {
       console.log('no coinciden')
+      cont2++
+      if (cont2 === 2) {
+        window.alert("LOOOSER!!")
+      }
     }
     console.log(matchCard[0])
     matchCard = []
@@ -46,8 +59,7 @@ function selectCard(card) {
 }
 // -------------------------------------------------
 
-// function frontCard (elem) {
-//   if (elem.target.innerText === '1') {
-//     elem.classList.add('ice-cream')
-//   }
-// }
+function winner () {
+  let cont = 0
+
+}
