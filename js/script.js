3d4a10979c3c609046ae082fcb05bf5935a3c9c1
Karol Ariza <karolarizag@gmail.com>
2021-05-25 10:42:50 +0100

    flipcards (incomplete)

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const arrCards = ['ice-cream', 'waffle', 'pancake', 'donut', 'ice-cream', 'waffl
 let matchCard = [] // Selected card pair
 
 
+// --------------------------------------------
+
 function setLevel (level) { // damos colores a las cartas
   let freePositions = [0, 1, 2, 3, 4, 5, 6, 7]
 
@@ -24,37 +26,58 @@ function setLevel (level) { // damos colores a las cartas
   })
 }
 
-setLevel(arrCards)
-
 
 // ----------------------------------------------
 
 
-let cont = 0
-let cont2 = 2
+let contWinner = 0
+let contLoser = 2
 function selectCard (card) {
   matchCard.push(card)
   if (matchCard.length === 2) {
     matchCard[0].style.pointerEvents = 'auto'
+    matchCard[1].classList.toggle('initial')
     if (matchCard[0].getAttribute('class') === matchCard[1].getAttribute('class')) {
       console.log('coinciden')
-      cont++
-      if (cont === 4) {
-        window.alert("YOU'RE ON FIRE!!!")
+      contWinner++
+      if (contWinner === 4) {
+        window.alert("WINNEEER!!!")
       }
       matchCard[0].style.pointerEvents = 'none'
       matchCard[1].style.pointerEvents = 'none'
     } else {
+      var timerId = setTimeout(flipCards, 500, matchCard)
       console.log('no coinciden')
-      cont2--
-      if (cont2 === 0) {
+      contLoser--
+      if (contLoser === 0) {
         window.alert("LOOOSER!!")
+        reset(cards)
+        clearTimeout(timerId)
+        setLevel(arrCards)
       }
     }
     console.log(matchCard[0])
     matchCard = []
   } else if (matchCard.length === 1) {
     matchCard[0].style.pointerEvents = 'none'
+    matchCard[0].classList.toggle('initial')
   }
 }
 // -------------------------------------------------
+
+function flipCards (arr) {
+  arr.forEach(elem => {
+    elem.classList.toggle('initial')
+  })
+}
+
+var button = document.getElementById('btn-start')
+button.onclick = function () {
+  setLevel(arrCards)
+  var timerId = setTimeout(flipCards, 2000, cards)
+}
+
+function reset (arr) {
+  arr.forEach(elem => elem.classList = '')
+
+}
