1a28a6b5c41010440bcd40db1374fb2557915d83
Augusto Hernandez <augustojhhdez@gmail.com>
2021-05-26 12:35:23 +0100

    waiting time and move racoon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+let interval
 
 const cards = document.querySelectorAll('[id^="img"]') // DOM card placeholders
+cards.forEach(function (elem) {
+  elem.style.pointerEvents = 'none'
+})
 
 const arrCards = ['ice-cream', 'waffle', 'pancake', 'donut', 'ice-cream', 'waffle', 'pancake', 'donut'] // Card combination
 
@@ -15,7 +19,7 @@ let contLoser = 2
 
 // --------------------------------------------
 
-function setLevel (level) { // damos colores a las cartas
+function setLevel(level) { // damos colores a las cartas
   const freePositions = [0, 1, 2, 3, 4, 5, 6, 7]
 
   for (let i = 0; i < level.length; i++) {
@@ -35,27 +39,25 @@ function setLevel (level) { // damos colores a las cartas
 
 // ----------------------------------------------
 
-function selectCard (card) {
+function selectCard(card) {
   matchCard.push(card)
   if (matchCard.length === 2) {
-    matchCard[0].style.pointerEvents = 'auto'
+    //  matchCard[0].style.pointerEvents = 'auto'
     matchCard[1].classList.toggle('initial')
     if (matchCard[0].getAttribute('class') === matchCard[1].getAttribute('class')) {
       console.log('coinciden')
       contWinner++
-      if (contWinner === 4) {
-        window.alert('WINNEEER!!!')
-      }
       matchCard[0].style.pointerEvents = 'none'
       matchCard[1].style.pointerEvents = 'none'
+      if (contWinner === 4) {
+        window.alert('WINNEEER!!!')
+        resetLevel(arrCards)
+      }
     } else {
       const timerId = setTimeout(flipCards, 500, matchCard)
       console.log('no coinciden')
       contLoser--
-      if (contLoser === 0) {
-        window.alert('LOOOSER!!')
-        resetLevel(timerId)
-      }
+      // if (contLoser === 0) { lose() }
     }
     console.log(matchCard[0])
     matchCard = []
@@ -65,22 +67,31 @@ function selectCard (card) {
   }
 }
 
-function flipCards (arr) {
+function flipCards(arr) {
   arr.forEach(elem => {
     elem.classList.toggle('initial')
+    if (elem.classList.value.includes('initial')) {
+      elem.style.pointerEvents = 'auto'
+      console.log('"inital state" - clickable! -->', elem)
+    } else { elem.style.pointerEvents = 'none'; console.log('NOT clickable! -->', elem) }
   })
+  let speed = 20
+  console.log('speed -->', speed)
+  if (arr === cards) { interval = setInterval(moveBox, 20) }
 }
 
-function resetLevel (timerId) {
+function resetLevel(timerId) {
   contWinner = 0
   contLoser = 2
   reset(cards)
   clearTimeout(timerId)
   setLevel(arrCards)
   setTimeout(flipCards, 2000, cards)
+  characterLeft = 520
+  barWidth = 100
 }
 
-function reset (arr) {
+function reset(arr) {
   arr.forEach(elem => {
     let classes = elem.getAttribute('class').split(' ')
     elem.classList.remove(classes[0])
@@ -92,9 +103,8 @@ function reset (arr) {
 
 // -------------------------------------------------
 
-const button = document.getElementById('btn-start')
-button.onclick = function () {
-  let interval = setInterval(moveBox, 20)
+const buttonStart = document.getElementById('btn-start')
+buttonStart.onclick = function () {
   setLevel(arrCards)
   const timerId = setTimeout(flipCards, 2000, cards)
 }
@@ -110,11 +120,26 @@ pause.onclick = function () {
 // ------------------------------------------------
 
 const character = document.getElementById('character')
+const bar = document.getElementById('fullness-bar')
 let characterLeft = 520
+let barWidth = character.style.left
 
-const moveBox = function () {
+const moveBox = function (timerId) {
   if (characterLeft > -20) {
     characterLeft -= 1
+  } else {
+    clearInterval(interval)
+    lose()
   }
   character.style.left = characterLeft + 'px'
+  bar.style.width = (parseInt(character.style.left.match(/\d+/)) + 30) + 'px'
 }
+
+// -----------------LOSE-FUNCTION-------------------
+
+function lose(timerId) {
+  window.alert('LOOOSER!!')
+  resetLevel(timerId)
+}
+
+resetBar()
